d963066366100dba91dc05d69447695c3f01b1ef
hema <sphema25@gmail.com>
2026-06-07 21:23:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
-function searchRemedy(){
 
-let input =
-document.getElementById("searchBar")
-.value.toLowerCase();
 
-let cards =
-document.querySelectorAll(".card");
-
-cards.forEach(card => {
-
-let text =
-card.innerText.toLowerCase();
-
-if(text.includes(input)){
-card.style.display="block";
-}
-else{
-card.style.display="none";
-}
-
-});
-
-}
